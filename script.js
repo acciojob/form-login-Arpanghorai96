2c8a71459228fd9c48f06d0d62270ce4a2d8d47d
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-05-04 01:36:40 -0700

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function getFormvalue() {
     //Write your code here
 	let firstName=document.getElementsByName("fname");
 	let lastName=document.getElementsByName("lname");
-		alert(firstName+" "+lastName)
+		alert(firstName[0].value+" "+lastName[0].value)
 
 }
